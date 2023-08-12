b09ef9c686eada1c0ae4436f789c8836e4350e70
binyamolango <binyamolango@github.com>
2023-08-12 21:41:51 +0300

    Fix api fetching

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import './style.css';
-import { disScores, add, scoObj } from './score.js';
+import { disScores, add } from './score.js';
 
 const form1 = document.getElementById('form');
 const name1 = document.getElementById('user');
 const scoreId1 = document.getElementById('score');
+const refBtn = document.getElementById('refresh');
 
-const addValue = (e) => {
+const addValue = async (e) => {
   e.preventDefault();
   if (!name1.value || !scoreId1.value) return;
-  add({ user: name1.value, score: scoreId1.value });
+  await add({ user: name1.value, score: scoreId1.value });
   name1.value = '';
   scoreId1.value = '';
-  disScores(scoObj.scores);
+};
+
+const disAPI = async () => {
+  const fetchAPI = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/L5KWB7dBUtngsInWODu0/scores/');
+  const data = await fetchAPI.json();
+  disScores(data.result);
 };
 
 form1.addEventListener('submit', addValue);
+refBtn.addEventListener('click', disAPI);
